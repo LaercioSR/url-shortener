08b7fe63687611b7b5091fb505037b666f53f35d
Laercio Rios <contact@laerciorios.com>
2025-04-09 23:48:03 -0300

    docs: add swagger config

---
+++ src/config/swagger.ts
@@ -0,0 +1,9 @@
+const swaggerOptions = {
+  customCss: `
+    .swagger-ui .topbar { 
+      display: none
+    }
+  `,
+  customSiteTitle: "URL Shortener API Documentation",
+};
+export default swaggerOptions;
--- src/infrastructure/http/server/buildApp.ts
+++ src/infrastructure/http/server/buildApp.ts
@@ -2,12 +2,21 @@ import "reflect-metadata";
 
 import cors from "cors";
 import express from "express";
+import swaggerFile from "../../../../swagger.json";
+import swaggerOptions from "../../../config/swagger";
+import swaggerUi from "swagger-ui-express";
 
 export async function buildApp() {
   const app = express();
 
   app.use(express.json());
   app.use(cors());
+
+  app.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerFile, swaggerOptions),
+  );
 
   return app;
 }
